c321cd9a30c5d4523b3728770d7ea7659ae34917
lucasmzz <lucasderh@gmail.com>
2023-03-18 21:41:09 +0100

    improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   BoltIcon,
   ExclamationTriangleIcon,
@@ -10,7 +9,7 @@ function HomePage() {
     <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
       <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
 
-      <div className="flex space-x-2 text-center">
+      <div className="flex flex-col spacey-2 md:flex-row space-x-2 text-center">
         <div className="">
           <div className="flex flex-col items-center justify-center mb-5">
             <SunIcon className="h-8 w-8" />
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -20,7 +20,7 @@ function Sidebar() {
 
   return (
     <div className="p-2 flex flex-col h-screen">
-      <div className="flex-1 ">
+      <div className="flex-1">
         <div className="">
           <NewChat />
 
